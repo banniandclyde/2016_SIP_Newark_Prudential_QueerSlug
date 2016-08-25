1b43a2702de079ec4e1793c2e8b8b17df2963666
douglasbrianne <douglasbrianne@gmail.com>
2016-08-25 16:49:42 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function saySomething(){
+	alert("Banni likes butts");
+}
+saySomething();
+function welcome(){
+	alert("Welcome to my website...I too like butts");
+}
+welcome();
---
+++ queer_java.js
@@ -0,0 +1,6 @@
+/*<script type="text/javascript">
+    document.getElementById('frmSearch').onsubmit = function() {
+        window.location = 'http://www.google.com/search?q=site:.com ' + document.getElementById('txtSearch').value;
+        return false;
+    }
+</script>*/
